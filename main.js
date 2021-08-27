2da418b6b37349cccfbea21a6f5d66d4738f8876
MinSeoke-Kim <60463011+MinSeoke-Kim@users.noreply.github.com>
2021-08-27 03:36:53 +0100

    main.js run on canvas
    .js run on canvas + 
    starvertices +

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import vertex from './shader/vertex.glsl'
 import fragment from './shader/fragment.glsl'
 import atmovertex from './shader/atmovertex.glsl'
 import atmofragment from './shader/atmofrag.glsl'
+import gsap from './node_modules/gsap'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
@@ -14,14 +15,13 @@ const camera = new THREE.PerspectiveCamera(
 
 const renderer = new THREE.WebGLRenderer(
   {
-    antialias: true
+    antialias: true,
+    canvas: document.querySelector('canvas')
   }
 )
 
-
 renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(window.devicePixelRatio)
-document.body.appendChild(renderer.domElement)
 
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
@@ -50,6 +50,26 @@ atmosphere.scale.set(1.1, 1.1, 1.1)
 
 scene.add(atmosphere)
 
+const starGeometry = new THREE.BufferGeometry()
+const starMaterial = new THREE.PointsMaterial({
+  color: 0xffffff
+})
+
+const starVertices = []
+for (let i = 0; i < 10000; i++) {
+  const x = (Math.random() - 0.5) * 2000
+  const y = (Math.random() - 0.5) * 2000
+  const z = -Math.random() * 2000
+  starVertices.push(x, y, z)
+}
+
+starGeometry.setAttribute('position',
+  new THREE.Float32BufferAttribute(starVertices, 3))
+
+const stars = new THREE.Points(
+  starGeometry, starMaterial)
+scene.add(stars)
+
 const group = new THREE.Group()
 group.add(sphere)
 scene.add(group)
@@ -65,7 +85,10 @@ function animate(){
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
   sphere.rotation.y += 0.001
-  group.rotation.y = mouse.x * 0.5
+  gsap.to(group.rotation,
+   {y: mouse.x * 0.5,
+    duration: 2
+  })
 }
 
 animate()
