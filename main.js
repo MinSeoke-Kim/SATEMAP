698da3358f469be848c7797aec13edfc305be812
MinSeoke-Kim <60463011+MinSeoke-Kim@users.noreply.github.com>
2021-08-27 03:09:11 +0100

    atmo glsl update
    atmosphere glsl + 
    mousemovement +

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js'
 import vertex from './shader/vertex.glsl'
 import fragment from './shader/fragment.glsl'
+import atmovertex from './shader/atmovertex.glsl'
+import atmofragment from './shader/atmofrag.glsl'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
@@ -34,13 +36,41 @@ const sphere = new THREE.Mesh(
   })
 )
 
-scene.add(sphere)
+const atmosphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.ShaderMaterial({
+    vertexShader: atmovertex,
+    fragmentShader: atmofragment,
+    blending: THREE.AdditiveBlending,
+    side: THREE.BackSide
+  })
+)
+
+atmosphere.scale.set(1.1, 1.1, 1.1)
+
+scene.add(atmosphere)
+
+const group = new THREE.Group()
+group.add(sphere)
+scene.add(group)
 
 camera.position.z = 15 
+
+const mouse = {
+  x: undefined,
+  y: undefined
+}
 
 function animate(){
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
+  sphere.rotation.y += 0.001
+  group.rotation.y = mouse.x * 0.5
 }
 
 animate()
+
+addEventListener('mousemove', () => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1
+})
