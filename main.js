de2c595f20f8332a3b62017fbaf2d36259f78ee1
MinSeoke-Kim <60463011+MinSeoke-Kim@users.noreply.github.com>
2021-08-27 01:28:06 +0100

    import THREE.js and Globe
    added webGL globe with the uv map.

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js'
 
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
-const renderer = new THREE.WebGLRenderer()
+const camera = new THREE.PerspectiveCamera(
+  75, 
+  innerWidth / innerHeight, 
+  0.1, 
+  1000
+)
 
-console.log(scene)
-console.log(camera)
-console.log(renderer)
+const renderer = new THREE.WebGLRenderer()
 
 renderer.setSize(innerWidth, innerHeight)
 document.body.appendChild(renderer.domElement)
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load('./img/globe.jpg')
+  })
+)
+
+scene.add(sphere)
+
+camera.position.z = 10
+
+function animate(){
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+}
+
+animate()
