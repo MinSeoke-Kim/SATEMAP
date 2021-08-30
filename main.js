270630674b9274d793819707594eebfac3584204
MinSeoke-Kim <60463011+MinSeoke-Kim@users.noreply.github.com>
2021-08-30 05:29:31 +0100

    mesh geometry changed
    sphere -> octahedron

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ const camera = new THREE.PerspectiveCamera(
   1,
   10000
 );
-camera.position.set(0, 0, 10);
+// set on U.S.A
+camera.position.set(-2, 6, 13);
 
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.update();
@@ -83,60 +84,61 @@ scene.add(stars);
 
 //--
 let SLC40 = new THREE.Mesh(
-  new THREE.SphereGeometry(0.04, 30, 30),
-  new THREE.MeshBasicMaterial({color:0xff004D})
+  new THREE.OctahedronGeometry(0.04),
+  new THREE.MeshBasicMaterial({color:0x00ff00})
 );
 
 let SLC40lat = (28.7617) * Math.PI / 180;
 let SLC40lng = (72.3918) * Math.PI / 180;
 
-let SLC40x = Math.cos(SLC40lng) * Math.sin(SLC40lat) * 5;
-let SLC40y = Math.sin(SLC40lng) * Math.sin(SLC40lat) * 5; 
-let SLC40z = Math.cos(SLC40lat) * 5;
+let SLC40x = Math.cos(SLC40lng) * Math.sin(SLC40lat) * 5.04;
+let SLC40y = Math.sin(SLC40lng) * Math.sin(SLC40lat) * 5.04; 
+let SLC40z = Math.cos(SLC40lat) * 5.04;
 SLC40.position.set(SLC40x, SLC40y, SLC40z);
 scene.add(SLC40)
 
 let SLC4E = new THREE.Mesh(
-  new THREE.SphereGeometry(0.04, 30, 30),
-  new THREE.MeshBasicMaterial({color:0xff004D})
+  new THREE.OctahedronGeometry(0.04),
+  new THREE.MeshBasicMaterial({color:0x00ff00})
 );
 
 let SLC4Elat = (43.8522) * Math.PI / 180;
 let SLC4Elng = (125.6437) * Math.PI / 180;
 
-let SLC4Ex = Math.cos(SLC4Elng) * Math.sin(SLC4Elat) * 5;
-let SLC4Ey = Math.sin(SLC4Elng) * Math.sin(SLC4Elat) * 5; 
-let SLC4Ez = Math.cos(SLC4Elat) * 5;
+let SLC4Ex = Math.cos(SLC4Elng) * Math.sin(SLC4Elat) * 5.04;
+let SLC4Ey = Math.sin(SLC4Elng) * Math.sin(SLC4Elat) * 5.04; 
+let SLC4Ez = Math.cos(SLC4Elat) * 5.04;
 SLC4E.position.set(SLC4Ex, SLC4Ey, SLC4Ez);
 scene.add(SLC4E)
 
 let LC39A = new THREE.Mesh(
-  new THREE.SphereGeometry(0.04, 30, 30),
-  new THREE.MeshBasicMaterial({color:0xff004D})
+  new THREE.OctahedronGeometry(0.04),
+  new THREE.MeshBasicMaterial({color:0x00ff00})
 );
 
 let LC39Alat = (29.7617) * Math.PI / 180;
 let LC39Alng = (73.3918) * Math.PI / 180;
 
-let LC39Ax = Math.cos(LC39Alng) * Math.sin(LC39Alat) * 5;
-let LC39Ay = Math.sin(LC39Alng) * Math.sin(LC39Alat) * 5; 
-let LC39Az = Math.cos(LC39Alat) * 5;
+let LC39Ax = Math.cos(LC39Alng) * Math.sin(LC39Alat) * 5.04;
+let LC39Ay = Math.sin(LC39Alng) * Math.sin(LC39Alat) * 5.04; 
+let LC39Az = Math.cos(LC39Alat) * 5.04;
 LC39A.position.set(LC39Ax, LC39Ay, LC39Az);
 scene.add(LC39A)
 
 let BOCA = new THREE.Mesh(
-  new THREE.SphereGeometry(0.04, 30, 30),
-  new THREE.MeshBasicMaterial({color:0xff004D})
+  new THREE.OctahedronGeometry(0.04),
+  new THREE.MeshBasicMaterial({color:0x00ff00})
 );
 
 let BOCAlat = (27.2017) * Math.PI / 180;
 let BOCAlng = (105.0918) * Math.PI / 180;
 
-let BOCAx = Math.cos(BOCAlng) * Math.sin(BOCAlat) * 5;
-let BOCAy = Math.sin(BOCAlng) * Math.sin(BOCAlat) * 5; 
-let BOCAz = Math.cos(BOCAlat) * 5;
+let BOCAx = Math.cos(BOCAlng) * Math.sin(BOCAlat) * 5.04;
+let BOCAy = Math.sin(BOCAlng) * Math.sin(BOCAlat) * 5.04; 
+let BOCAz = Math.cos(BOCAlat) * 5.04;
 BOCA.position.set(BOCAx, BOCAy, BOCAz);
-scene.add(BOCA)
+scene.add(BOCA);
+
 
 
 //--Animation 
